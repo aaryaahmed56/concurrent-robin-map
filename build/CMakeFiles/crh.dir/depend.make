132b34adad0de590bfd3ed90eb9c5ef35d0433ae
Asef Ahmed <asefahmed56@pop-os.localdomain>
2020-02-15 01:03:11 -0500

    remove cmake build

--- build/CMakeFiles/crh.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for crh.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asefahmed56/Desktop/Projects/c_robin_map
-# Build directory: /home/asefahmed56/Desktop/Projects/c_robin_map/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
